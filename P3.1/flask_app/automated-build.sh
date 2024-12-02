572320fe67f7e0157039d95e8c1bcf9422b88ba5
hesitorremitos <hesitorremitos@gmail.com>
2024-12-02 22:14:47 +0000

    Automated

--- P3.1/flask_app/automated-build.sh
+++ P3.1/flask_app/automated-build.sh
@@ -5,4 +5,6 @@ echo "EXPOSE 5000" >> Dockerfile
 # Ejecutar al instalarS
 echo "CMD python3 /home/myapp/main.py" >> Dockerfile
 
-docker build -t examen .
+docker build -t examen_final .
+docker run -t -d -p 5000:5000 --name examen_running examen_final
+docker ps -a
